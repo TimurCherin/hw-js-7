ad41ba101bbafdd86ce7f2a0f0cddfc292c020dc
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-05-06 09:05:43 +0300

    hw-js-7

--- js/index.js
+++ js/index.js
@@ -1,216 +1,226 @@
-                                                    // ex 1
 let lang = "ua";
 switch (lang) {
-    case "ua":
-        console.log("Січень");
-        break;
+  case "ua":
+    console.log("Січень");
+    break;
 
-    case "fr":
-        console.log("Janvier");
-        break;
+  case "fr":
+    console.log("Janvier");
+    break;
 
-    case "en":
-        console.log("January");
-        break;
+  case "en":
+    console.log("Janvier");
+    break;
 
-    case "ger":
-        console.log("Januar");
-        break;
+  case "ger":
+    console.log("Januar");
+    break;
 
-        default:
-            console.log("Ви не вибрали мову")
+  default:
+    console.log("Ви не вибрали мову");
 }
-                                                        // ex 2
-const drink = prompt("Виберіть напій");
+// ex 2
+const drink = prompt("Виберіть напій").toLocaleLowerCase();
 let cost;
 switch (drink) {
-    case "сік":
-        cost = 8;
-        console.log(`Ви вибрали сік, до сплати ${cost} грн`);
-        break;
+  case "сік":
+    cost = 8;
+    console.log(`Ви вибрали сік, до сплати ${cost} грн`);
+    break;
 
-    case "чай":
-        cost = 10;
-        console.log(`Ви вибрали чай, до сплати ${cost} грн`);
-        break;
+  case "чай":
+    cost = 10;
+    console.log(`Ви вибрали чай, до сплати ${cost} грн`);
+    break;
 
-    case "кава":
-        cost = 12;
-        console.log(`Ви вибрали каву, до сплати ${cost} грн`);
-        break;
-        
-        default:
-            console.log("Ви не вибрали напій");
+  case "кава":
+    cost = 12;
+    console.log(`Ви вибрали каву, до сплати ${cost} грн`);
+    break;
+
+  default:
+    console.log("Ви не вибрали напій");
 }
-                                                        // ex 3
-const question = (prompt("Який день тижня?")).toLocaleLowerCase();
+// ex 3
+const question = prompt("Який день тижня?").toLocaleLowerCase();
 let day;
 switch (question) {
-    case "понеділок":
-        day = "робочий";
-        console.log(day);
-        break;
-    
-    case "вівторок":
-        day = "робочий";
-        console.log(day);
-        break;
+  case "понеділок":
+    day = "робочий";
+    console.log(day);
+    break;
 
-    case "середа":
-        day = "робочий";
-        console.log(day);
-        break;
-    
-    case "четвер":
-        day = "робочий";
-        console.log(day);
-        break;
-    
-    case "п'ятниця":
-        day = "робочий";
-        console.log(day);
-        break;
+  case "вівторок":
+    day = "робочий";
+    console.log(day);
+    break;
 
-    case "субота":
-        day = "вихідний";
-        console.log(day);
-        break;
-    
-    case "неділя":
-        day = "вихідний";
-        console.log(day);
-        break;
+  case "середа":
+    day = "робочий";
+    console.log(day);
+    break;
 
-        default:
-            console.log("Ви не ввели день тижня");
+  case "четвер":
+    day = "робочий";
+    console.log(day);
+    break;
+
+  case "п'ятниця":
+    day = "робочий";
+    console.log(day);
+    break;
+
+  case "субота":
+    day = "вихідний";
+    console.log(day);
+    break;
+
+  case "неділя":
+    day = "вихідний";
+    console.log(day);
+    break;
+
+  default:
+    console.log("Ви не ввели день тижня");
 }
-                                                        // ex 4
-const month = prompt("Введіть місяць");
+// ex 4
+const month = Number(prompt("Який номер поточного місяця?"));
 let season;
 switch (month) {
-    case "грудень":
-        season = "winter";
-        console.log(season);
-        break;
+  case 12:
+    season = "winter";
+    console.log(season);
+    break;
 
-    case "січень":
-        season = "winter";
-        console.log(season);
-        break;
+  case 1:
+    season = "winter";
+    console.log(season);
+    break;
 
-    case "лютий":
-        season = "winter";
-        console.log(season);
-        break;
+  case 2:
+    season = "winter";
+    console.log(season);
+    break;
 
-    case "березень":
-        season = "spring";
-        console.log(season);
-        break;
+  case 3:
+    season = "spring";
+    console.log(season);
+    break;
 
-    case "квітень":
-        season = "spring";
-        console.log(season);
-        break;
+  case 4:
+    season = "spring";
+    console.log(season);
+    break;
 
-    case "травень":
-        season = "spring";
-        console.log(season);
-        break;
+  case 5:
+    season = "spring";
+    console.log(season);
+    break;
 
-    case "червень":
-        season = "sumer";
-        console.log(season);
-        break;
-    
-    case "липень":
-        season = "sumer";
-        console.log(season);
-        break;
+  case 6:
+    season = "summer";
+    console.log(season);
+    break;
 
-    case "серпень":
-        season = "sumer";
-        console.log(season);
-        break;
+  case 7:
+    season = "summer";
+    console.log(season);
+    break;
 
-    case "вересень":
-        season = "autumn";
-        console.log(season);
-        break;
+  case 8:
+    season = "summer";
+    console.log(season);
+    break;
 
-    case "жовтень":
-        season = "autumn";
-        console.log(season);
-        break;
+  case 9:
+    season = "autumn";
+    console.log(season);
+    break;
 
-    case "листопад":
-        season = "autumn";
-        console.log(season);
-        break;
+  case 10:
+    season = "autumn";
+    console.log(season);
+    break;
+
+  case 11:
+    season = "autumn";
+    console.log(season);
+    break;
+
+  default:
+    console.log("Ви не ввели номер місяця");
 }
-                                                        // ex 5
-const tabl = alert("7 * 1 = 7, 7 * 2 = 14, 7 * 3 = 21, 7 * 4 = 28, 7 * 5 = 35, 7 * 6 = 42, 7 * 7 = 49, 7 * 8 = 56, 7 * 9 = 63, 7 * 10 = 70");
-                                                        // ex 6
+// ex 5
+let maxNumber = 20;
+for (let i = 1; i <= maxNumber; i++) {
+  let mult = i * 7;
+  console.log(`${i} * 7 = ${mult}`);
+} 
+
+// ex 6
 const min = 12;
 const max = 27;
 let total = 0;
 
-for (let i = min; i <= max; i++){
-    total = total + i;
-    console.log(total);
+for (let i = min; i <= max; i++) {
+  total = total + i;
 }
-                                                        // ex 7
-const maax = 9;
-const miin = 3;
-let totall = 1;
-for (let i = miin; i <= maax; i++) {
-    if (i % 2 === 1) {
-        totall = totall * i;
-        console.log(totall);
-    }
+console.log(`sum = ${total}`);
+// ex 7
+const max2 = 9;
+const min2 = 3;
+let totalNumber = 1;
+for (let i = min2; i <= max2; i++) {
+  if (i % 2 === 1) {
+    totalNumber = totalNumber * i;
+  }
 }
-                                                        // ex 8
-const maaax = 99;
-const miiin = 1;
-for (let i = miiin; i <= maaax; i++) {
-    if (i === 49){
-        break;
-    }
-    console.log(i);
+console.log(`totalNumber = ${totalNumber}`);
+// ex 8
+const maxNumb = 99;
+const minNumb = 1;
+for (let i = minNumb; i <= maxNumb; i++) {
+  if (i === 49) {
+    console.log("We find 47");
+    break;
+  }
+  console.log(i);
 }
-                                                        // ex 9
+// ex 9
 for (let i = 10; i <= 30; i++) {
-    if (i === 15){
-        continue;
-    }
-    console.log(i);
+  if (i === 15) {
+    continue;
+  }
+  console.log(i);
 }
-                                                        // ex 10
-let totalPrice = 0; 
+// ex 10
+let totalPrice = 0;
 const goods = 10;
 for (let i = 1; i <= goods; i += 1) {
-    const price = Math.round(Math.random() * (5000 - 500) + 500);
-    totalPrice = totalPrice + price;
+  const price = Math.round(Math.random() * (5000 - 500) + 500);
+  totalPrice = totalPrice + price;
 }
 console.log(totalPrice);
-                                                        // ex 11
-let counter = 1;
+// ex 11
+let counter1 = 1;
 
-while (counter <= 5) {
-    console.log(counter)
-    counter += 1;
+while (counter1 <= 5) {
+  console.log(counter1);
+  counter1 += 1;
 }
+
+let counter2 = 1;
+
+do {
+  console.log(counter2);
+  counter2 += 1;
+} while (counter2 <= 5);
 
 for (let i = 1; i <= 5; i += 1) {
-    console.log(i);
+  console.log(i);
 }
 
-let myNumber = 1;
-do {
-  console.log(myNumber);
-  myNumber += 1;
-} while (myNumber <= 5);
-                                                        // ex 12
+// ex 12
 let number;
 do {
   number = Number(prompt("Введіть число більше 100"));
@@ -218,36 +228,38 @@ do {
     break;
   }
 } while (number < 100);
-                                                        // Додаткове завдання
-let contry = (prompt("Введіть назву країни у яку бажаєте замовити доставку")).toLocaleLowerCase();
+
+// Додаткове завдання
+let contry = prompt(
+  "Введіть назву країни у яку бажаєте замовити доставку"
+).toLocaleLowerCase();
 let costt;
-switch (contry){
-    case "китай":
+switch (contry) {
+  case "китай":
     costt = 100;
     console.log(`Доставка в ${contry} буде коштувати ${costt} кредитів`);
     break;
 
-    case "чилі":
+  case "чилі":
     costt = 250;
     console.log(`Доставка в ${contry} буде коштувати ${costt} кредитів`);
     break;
 
-    case "австралія":
+  case "австралія":
     costt = 170;
     console.log(`Доставка в ${contry} буде коштувати ${costt} кредитів`);
     break;
 
-    case "індія":
+  case "індія":
     costt = 80;
     console.log(`Доставка в ${contry} буде коштувати ${costt} кредитів`);
     break;
 
-    case "ямайка":
+  case "ямайка":
     costt = 120;
     console.log(`Доставка в ${contry} буде коштувати ${costt} кредитів`);
     break;
 
-    default:
-        const message = alert("У вашій країні доставка недоступна")
+  default:
+    const message = alert("У вашій країні доставка недоступна");
 }
-
